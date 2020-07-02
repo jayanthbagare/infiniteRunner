6b92c9b1c332a1f4c87ce60b35de216ea77f1fe9
jayanth <jayanth.bagare@gmail.com>
2020-07-02 21:24:30 +0530

    Ground is added
    	new file:   assets/map1.png
    	new file:   assets/tileset/infiniteTileMap.png
    	modified:   src/scenes/GameScene.js

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -1,17 +1,25 @@
 class GameScene extends Phaser.Scene{
+
     constructor(){
+        var ground;
         super({
             key:'GameScene'
         });
     }
 
     preload(){
-        console.log('Inside Preload of GameScene');
+        this.load.image('ground','assets/map1.png');
         
     }
 
     create(){
+        var counter = 0;
 
+        while(counter < window.innerWidth -10 ){
+            this.ground = this.add.tileSprite(counter,window.innerHeight,1024,180,'ground');
+            counter += 1024;
+        }
+        
     }
 
     update(){
