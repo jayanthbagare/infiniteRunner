5b54ab3de8de9726c6b55a99059e3f38db3a7824
jayanth <jayanth.bagare@gmail.com>
2020-07-05 02:09:03 +0530

    Corrected ground scrolling

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -19,25 +19,36 @@ class GameScene extends Phaser.Scene{
     }
 
     create(){
-
-        var counter = 0;
-        this.add.tileSprite(counter,window.innerHeight - 100,window.innerWidth,109,"ground").setOrigin(0);
+        //Setup the platform
+        this.ground = this.add.tileSprite(0,window.innerHeight - 100,window.innerWidth,109,"ground").setOrigin(0).setScrollFactor(0);
+        this.physics.add.existing(this.ground);
         
-        this.player = this.physics.add.sprite(counter, window.innerHeight/2, 'player');
+        this.ground.body.immovable = true;
+        this.ground.body.collideWorldBounds = true;
+        this.ground.body.allowGravity = false;
+
+        //Setup the Player
+        this.player = this.physics.add.sprite(0, window.innerHeight/2, 'player');
         this.player.setCollideWorldBounds(true);
+        this.player.body.checkCollision.right = false
+
+        //Setup the Camera
+        this.cameras.main.startFollow(this.player);
+        this.cameras.main.setBounds(0,0,Number.MAX_SAFE_INTEGER,window.innerHeight - 100);
+
 
         this.physics.add.collider(this.player,this.ground);
 
         this.anims.create({
             key:'static',
             frames:[{key:'player',frame:0}],
-            frameRate:20
+            frameRate:9
         });
 
         this.anims.create({
             key:'run',
             frames:this.anims.generateFrameNumbers('player',{start:1,end:9}),
-            frameRate:9,
+            frameRate:8,
             repeat: -1
         });
 
@@ -47,13 +58,16 @@ class GameScene extends Phaser.Scene{
     }
     
     update(){
-            if(this.cursors.right.isDown){
-                this.player.setVelocityX(120);
-                this.player.anims.play("run",true);
-            }else{
-                this.player.setVelocityX(0);
-                this.player.anims.play('static');
-            }
+       if(this.cursors.right.isDown){
+            this.player.setVelocityX(6);
+            this.ground.tilePositionX += 6;
+            this.player.anims.play("run",true);
+        }
+        else{
+            this.player.setVelocityX(0);
+            this.player.anims.play('static');
+        }
+
            
     }
 }
